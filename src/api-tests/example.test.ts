3f6db7b20b0525da84196a91d64890864e7e38c1
Nikola Schou <nikola.schou@adeccogroup.com>
2021-04-08 08:51:42 +0200

    Add readme

--- src/api-tests/example.test.ts
+++ src/api-tests/example.test.ts
@@ -1,12 +1,12 @@
 import postman from '../postman';
-describe('Demonstrate how api-tests work', () => {
-  it('Demonstrate how to make a GET request', async () => {
+describe('A Preman example collection', () => {
+  it('Demonstrate how to make Preman GET request', async () => {
     const result = await postman.GET('http://dummy.restapiexample.com/api/v1/employees');
     //console.log(result.text);
     // do nothing
   });
 
-  it('Demonstrate how to make a POST request', async () => {
+  it('Demonstrate how to make Preman POST request', async () => {
     const result = await postman.POST('http://dummy.restapiexample.com/api/v1/create', {"name":"test","salary":"123","age":"23"});
     //console.log(result.text);
     // do nothing
