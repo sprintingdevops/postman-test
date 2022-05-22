68ae6d40777dff79f18764f8025a5b4ed8439329
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-05-22 13:10:28 +0300

    feat(exampels): add assertions

--- examples/example.test.ts
+++ examples/example.test.ts
@@ -1,16 +1,21 @@
 import postman from "../src/postman";
+
+const BASE_URL = "http://dummy.restapiexample.com/api/v1";
+
 describe("A Preman example collection", () => {
   it("Demonstrate how to make Preman GET request", async () => {
-    const result = await postman.GET(
-      "http://dummy.restapiexample.com/api/v1/employees"
-    );
+    const result = await postman.GET(`${BASE_URL}/employees`);
+    expect(result.body.status).toEqual("success");
+    expect(result.body.data.length).toBeGreaterThan(0);
   });
 
   it("Demonstrate how to make Preman POST request", async () => {
     const result = await postman.POST(
-      "http://dummy.restapiexample.com/api/v1/create",
+      `${BASE_URL}/create`,
       {},
       { name: "test", salary: "123", age: "23" }
     );
+    expect(result.status).toEqual(200);
+    expect(result.body.status).toEqual("success");
   });
 });
