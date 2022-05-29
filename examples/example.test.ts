3c30440201b7bdd3b3078f97e37366ffdf34aa27
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-05-29 13:45:36 +0300

    test: skip example tests since public API has throttling

--- examples/example.test.ts
+++ examples/example.test.ts
@@ -3,13 +3,13 @@ import postman from '../src/postman';
 const BASE_URL = 'http://dummy.restapiexample.com/api/v1';
 
 describe('A Preman example collection', () => {
-  it('Demonstrate how to make Preman GET request', async () => {
+  it.skip('Demonstrate how to make Preman GET request', async () => {
     const result = await postman.GET(`${BASE_URL}/employees`);
     expect(result.body.status).toEqual('success');
     expect(result.body.data.length).toBeGreaterThan(0);
   });
 
-  it('Demonstrate how to make Preman POST request', async () => {
+  it.skip('Demonstrate how to make Preman POST request', async () => {
     const result = await postman.POST(`${BASE_URL}/create`, {}, {name: 'test', salary: '123', age: '23'});
     expect(result.status).toEqual(200);
     expect(result.body.status).toEqual('success');
