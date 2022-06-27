da0e528c732bb8d540cf2b8311382a5b8123725e
Zlatin Stanimirov <zlatin.stanimirovv@gmail.com>
2022-06-27 19:50:13 +0300

    feat: runner supports more HTTP methods

--- src/runTests.ts
+++ src/runTests.ts
@@ -1,34 +1,51 @@
-import {client, TestSchema} from '.';
+import supertest from 'supertest';
+import {client, StadiusResponse, TestSchema} from '.';
 import validateTestSchemas from './validateTestSchemas';
 
 export default function runTests(tests: TestSchema[]) {
   validateTestSchemas(tests);
 
   it.each(tests)('Executing test: $name', async ({name, url, request, response}) => {
-    let actualResult;
-    if (request.method === 'GET') {
-      actualResult = await client.GET(url, request.headers);
-    } else {
-      actualResult = await client.POST(url, request.headers, request.body);
+    const actualResult = await getResult({name, url, request, response});
+    const expectedResponse = response;
+
+    if (expectedResponse.statusCode) {
+      expect(actualResult.statusCode).toEqual(expectedResponse.statusCode);
     }
 
-    if (response.statusCode) {
-      expect(actualResult.statusCode).toEqual(response.statusCode);
+    if (expectedResponse.body) {
+      validateBody(expectedResponse, actualResult);
     }
 
-    if (response.body) {
-      const bodyType = typeof response.body;
-      if (bodyType === 'object' && !Array.isArray(response.body)) {
-        expect(actualResult.body).toMatchObject(response.body);
-      } else if (bodyType === 'string') {
-        expect(actualResult.text).toContain(response.body);
-      } else {
-        expect(actualResult.body).toEqual(response.body);
-      }
-    }
-
-    if (response.headers) {
-      expect(actualResult.headers).toMatchObject(response.headers);
+    if (expectedResponse.headers) {
+      expect(actualResult.headers).toMatchObject(expectedResponse.headers);
     }
   });
 }
+
+const getResult = async ({url, request}: TestSchema) => {
+  if (request.method === 'GET') {
+    return client.GET(url, request.headers);
+  } else if (request.method === 'POST') {
+    return client.POST(url, request.headers, request.body);
+  } else if (request.method === 'PUT') {
+    return client.PUT(url, request.headers, request.body);
+  } else if (request.method === 'PATCH') {
+    return client.PATCH(url, request.headers, request.body);
+  } else if (request.method === 'DELETE') {
+    return client.DELETE(url, request.headers);
+  } else {
+    throw new Error(`Unknown HTTP verb: ${request.method}`);
+  }
+};
+
+const validateBody = (expectedResponse: StadiusResponse, actualResponse: supertest.Response) => {
+  const bodyType = typeof expectedResponse.body;
+  if (bodyType === 'object' && !Array.isArray(expectedResponse.body)) {
+    expect(actualResponse.body).toMatchObject(expectedResponse.body);
+  } else if (bodyType === 'string') {
+    expect(actualResponse.text).toContain(expectedResponse.body);
+  } else {
+    expect(actualResponse.body).toEqual(expectedResponse.body);
+  }
+};
